5363369de78e3c2bae40b9e0882d178f767ca793
Keyna Rafael <keynarafaelp@gmail.com>
2022-12-08 12:59:32 -0500

    cleaning and formatting

--- tap_capsulecrm/streams.py
+++ tap_capsulecrm/streams.py
@@ -1,27 +1,21 @@
 """Stream type classes for tap-capsulecrm."""
 
 from pathlib import Path
-from typing import Any, Dict, Optional, Union, List, Iterable
+from typing import Any, Dict, Iterable, List, Optional, Union
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_capsulecrm.client import CapsulecrmStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
-
 
 class PartiesStream(CapsulecrmStream):
     """Define custom stream."""
+
     name = "parties"
     path = "/parties"
     primary_keys = ["id"]
     replication_key = None
     records_jsonpath = "$.parties[*]"
-    # Optionally, you may also use `schema_filepath` in place of `schema`:
-    # schema_filepath = SCHEMAS_DIR / "users.json"
     schema = th.PropertiesList(
         th.Property("id", th.NumberType),
         th.Property("type", th.StringType),
@@ -32,60 +26,79 @@ class PartiesStream(CapsulecrmStream):
         th.Property("jobTitle", th.StringType),
         th.Property("createdAt", th.DateTimeType),
         th.Property("updatedAt", th.DateTimeType),
-        th.Property("organisation", th.ObjectType(
-            th.Property("id", th.IntegerType),
-            th.Property("name", th.StringType),
-            th.Property("pictureURL", th.StringType),
-            th.Property("username", th.StringType),
-        )),
+        th.Property(
+            "organisation",
+            th.ObjectType(
+                th.Property("id", th.IntegerType),
+                th.Property("name", th.StringType),
+                th.Property("pictureURL", th.StringType),
+                th.Property("username", th.StringType),
+            ),
+        ),
         th.Property("lastContactedAt", th.DateTimeType),
-        th.Property("owner", th.ObjectType(
-            th.Property("id", th.IntegerType),
-            th.Property("name", th.StringType),
-            th.Property("pictureURL", th.StringType),
-            th.Property("username", th.StringType),
-        )),
+        th.Property(
+            "owner",
+            th.ObjectType(
+                th.Property("id", th.IntegerType),
+                th.Property("name", th.StringType),
+                th.Property("pictureURL", th.StringType),
+                th.Property("username", th.StringType),
+            ),
+        ),
         th.Property("team", th.StringType),
-        th.Property("addresses", th.ArrayType(
-            th.ObjectType(
-                th.Property("id", th.NumberType),
-                th.Property("type", th.StringType),
-                th.Property("city", th.StringType),
-                th.Property("country", th.StringType),
-                th.Property("street", th.StringType),
-                th.Property("state", th.StringType),
-                th.Property("zip", th.StringType),
-            )
-        )),
-        th.Property("phoneNumbers", th.ArrayType(
-            th.ObjectType(
-                th.Property("id", th.NumberType),
-                th.Property("type", th.StringType),
-                th.Property("number", th.StringType),
-            )
-        )),
-        th.Property("websites", th.ArrayType(
-            th.ObjectType(
-                th.Property("id", th.NumberType),
-                th.Property("type", th.StringType),
-                th.Property("address", th.StringType),
-                th.Property("service", th.StringType),
-                th.Property("url", th.StringType),
+        th.Property(
+            "addresses",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.NumberType),
+                    th.Property("type", th.StringType),
+                    th.Property("city", th.StringType),
+                    th.Property("country", th.StringType),
+                    th.Property("street", th.StringType),
+                    th.Property("state", th.StringType),
+                    th.Property("zip", th.StringType),
+                )
             ),
-        )),
-        th.Property("emailAddresses", th.ArrayType(
-            th.ObjectType(
-                th.Property("id", th.NumberType),
-                th.Property("type", th.StringType),
-                th.Property("address", th.StringType),
+        ),
+        th.Property(
+            "phoneNumbers",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.NumberType),
+                    th.Property("type", th.StringType),
+                    th.Property("number", th.StringType),
+                )
             ),
-        )),
+        ),
+        th.Property(
+            "websites",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.NumberType),
+                    th.Property("type", th.StringType),
+                    th.Property("address", th.StringType),
+                    th.Property("service", th.StringType),
+                    th.Property("url", th.StringType),
+                ),
+            ),
+        ),
+        th.Property(
+            "emailAddresses",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("id", th.NumberType),
+                    th.Property("type", th.StringType),
+                    th.Property("address", th.StringType),
+                ),
+            ),
+        ),
         th.Property("pictureURL", th.StringType),
     ).to_dict()
 
 
 class OpportunitiesStream(CapsulecrmStream):
     """Define custom stream."""
+
     name = "opportunities"
     path = "/opportunities"
     primary_keys = ["id"]
@@ -95,26 +108,38 @@ class OpportunitiesStream(CapsulecrmStream):
         th.Property("id", th.NumberType),
         th.Property("updatedAt", th.DateTimeType),
         th.Property("description", th.StringType),
-        th.Property("owner", th.ObjectType(
-            th.Property("id", th.IntegerType),
-            th.Property("name", th.StringType),
-            th.Property("pictureURL", th.StringType),
-            th.Property("username", th.StringType),
-        )),
-        th.Property("party", th.ObjectType(
-            th.Property("id", th.NumberType),
-            th.Property("username", th.StringType),
-            th.Property("name", th.StringType),
-        )),
+        th.Property(
+            "owner",
+            th.ObjectType(
+                th.Property("id", th.IntegerType),
+                th.Property("name", th.StringType),
+                th.Property("pictureURL", th.StringType),
+                th.Property("username", th.StringType),
+            ),
+        ),
+        th.Property(
+            "party",
+            th.ObjectType(
+                th.Property("id", th.NumberType),
+                th.Property("username", th.StringType),
+                th.Property("name", th.StringType),
+            ),
+        ),
         th.Property("lostReason", th.StringType),
-        th.Property("milestone", th.ObjectType(
-            th.Property("id", th.NumberType),
-            th.Property("name", th.StringType),
-        )),
-        th.Property("value", th.ObjectType(
-            th.Property("amount", th.NumberType),
-            th.Property("currency", th.StringType),
-        )),
+        th.Property(
+            "milestone",
+            th.ObjectType(
+                th.Property("id", th.NumberType),
+                th.Property("name", th.StringType),
+            ),
+        ),
+        th.Property(
+            "value",
+            th.ObjectType(
+                th.Property("amount", th.NumberType),
+                th.Property("currency", th.StringType),
+            ),
+        ),
         th.Property("expectedCloseOn", th.DateTimeType),
         th.Property("probability", th.NumberType),
         th.Property("durationBasis", th.StringType),
--- tap_capsulecrm/tap.py
+++ tap_capsulecrm/tap.py
@@ -3,15 +3,14 @@
 from typing import List
 
 from singer_sdk import Tap, Stream
-from singer_sdk import typing as th  # JSON schema typing helpers
-# TODO: Import your custom stream types here:
+from singer_sdk import typing as th
+
 from tap_capsulecrm.streams import (
     CapsulecrmStream,
     PartiesStream,
     OpportunitiesStream,
 )
-# TODO: Compile a list of custom stream types here
-#       OR rewrite discover_streams() below with your custom logic.
+
 STREAM_TYPES = [
     PartiesStream,
     OpportunitiesStream,
@@ -20,26 +19,26 @@ STREAM_TYPES = [
 
 class TapCapsulecrm(Tap):
     """Capsulecrm tap class."""
+
     name = "tap-capsulecrm"
 
-    # TODO: Update this section with the actual config values you expect:
     config_jsonschema = th.PropertiesList(
         th.Property(
             "auth_token",
             th.StringType,
             required=True,
-            description="The token to authenticate against the API service"
+            description="The token to authenticate against the API service",
         ),
         th.Property(
             "start_date",
             th.DateTimeType,
-            description="The earliest record date to sync"
+            description="The earliest record date to sync",
         ),
         th.Property(
             "api_url",
             th.StringType,
             default="https://api.capsulecrm.com/api/v2",
-            description="The url for the API service"
+            description="The url for the API service",
         ),
     ).to_dict()
 
@@ -47,5 +46,6 @@ class TapCapsulecrm(Tap):
         """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     TapCapsulecrm.cli()
